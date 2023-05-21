05827fc5beb5a359a48d03dbdcabf93f9fa01b78
gemstone <toptowermilan@gmail.com>
2023-05-21 09:07:27 -0700

    Add chat and notifications to front end

--- chess-frontend/src/components/ChatSection.svelte
+++ chess-frontend/src/components/ChatSection.svelte
@@ -1,1 +1,18 @@
-<div></div>
+<div>
+    <iframe src='https://streamelements.com/overlay/646a3cbc2ca37d07c5ca1dc3/7d4Q4tdv5-oQotZPkdJGOxItNYS6Ns3pKHj3jLhkx-6KdTr9' />
+    <iframe src='https://streamelements.com/overlay/646a3e182ca37dcc54ca27b4/xrnLMEzwi054absQd4yhevlewWASu1aaReLGEhUq4FNotmck' />
+    <iframe src='https://streamelements.com/overlay/646a4007822a3f6f1021a462/7d4Q4tdv5-oQotZPkdJGOxItNYS6Ns3pKHj3jLhkx-6KdTr9' />
+</div>
+
+<style>
+    div {
+        display: grid;
+        gap: 1rem;
+        grid-template-rows: 1fr 250px 175px;
+    }
+    
+    iframe {
+        width: 100%;
+        height: 100%;
+    }
+</style>
